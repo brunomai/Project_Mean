9cfdc70c2b48cdd560413320217311d03bbdd3e2
brunomai <brunomai14@gmail.com>
2019-09-13 13:08:58 -0300

    Versão v1.0

---
+++ controllers/questions.controller.js
@@ -0,0 +1,25 @@
+﻿var express = require('express');
+var router = express.Router();
+var questionService = require('services/question.service');
+var config = require('config.json');
+
+// router.get('/getAll', function (req, res) {
+//     res.render('register');
+// });
+
+router.post('/create', registerQuestion);
+
+module.exports = router;
+
+function registerQuestion(req,res){
+    questionService.create(req.body)
+    .then(function(){
+
+    })
+    // .catch(function(err))
+    // {
+    //         //fsdfsf
+    // }
+}
+
+
