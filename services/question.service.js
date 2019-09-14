7a2297f5ec0dab4e342e750dde16ee2e2b85f0d6
brunomai <brunomai14@gmail.com>
2019-09-14 17:04:33 -0300

    Versão Final

--- services/question.service.js
+++ services/question.service.js
@@ -9,94 +9,37 @@ db.bind('questions');
 
 var service = {};
 
-
-// service.getById = getById;
-service.create = create;
-// service.update = update;
-// service.delete = _delete;
+service.GetAll = GetAll;
+service.Create = Create;
 
 module.exports = service;
 
+function GetAll() {
 
-
-function create(question) {
     var deferred = Q.defer();
-
-    // validation
-    db.questions.insert({
-        question,
-        function (err,doc) {
-            if(err) deferred.reject(err.name + ': ' + err.message);
-
+    db.questions.find().toArray(function(err, items) {
+        if (items) {  
+            deferred.resolve(items);
+        } else {            
             deferred.resolve();
         }
+        console.log(deferred.promise)
     });
+    return deferred.promise;
+    
+}
+
+function Create(question) {
+    var deferred = Q.defer();
+
+    db.questions.insert(
+        question,
+        function (err, doc) {
+            if (err) deferred.reject(err.name + ': ' + err.message);
+
+            deferred.resolve();
+        });
+    
 
     return deferred.promise;
 }
-
-// function update(_id, userParam) {
-//     var deferred = Q.defer();
-
-//     // validation
-//     db.users.findById(_id, function (err, user) {
-//         if (err) deferred.reject(err.name + ': ' + err.message);
-
-//         if (user.username !== userParam.username) {
-//             // username has changed so check if the new username is already taken
-//             db.users.findOne(
-//                 { username: userParam.username },
-//                 function (err, user) {
-//                     if (err) deferred.reject(err.name + ': ' + err.message);
-
-//                     if (user) {
-//                         // username already exists
-//                         deferred.reject('Username "' + req.body.username + '" is already taken')
-//                     } else {
-//                         updateUser();
-//                     }
-//                 });
-//         } else {
-//             updateUser();
-//         }
-//     });
-
-//     function updateUser() {
-//         // fields to update
-//         var set = {
-//             firstName: userParam.firstName,
-//             lastName: userParam.lastName,
-//             username: userParam.username,
-//         };
-
-//         // update password if it was entered
-//         if (userParam.password) {
-//             set.hash = bcrypt.hashSync(userParam.password, 10);
-//         }
-
-//         db.users.update(
-//             { _id: mongo.helper.toObjectID(_id) },
-//             { $set: set },
-//             function (err, doc) {
-//                 if (err) deferred.reject(err.name + ': ' + err.message);
-
-//                 deferred.resolve();
-//             });
-//     }
-
-//     return deferred.promise;
-// }
-
-// function _delete(_id) {
-//     var deferred = Q.defer();
-
-//     db.users.remove(
-//         { _id: mongo.helper.toObjectID(_id) },
-//         function (err) {
-//             if (err) deferred.reject(err.name + ': ' + err.message);
-
-//             deferred.resolve();
-//         });
-
-//     return deferred.promise;
-// }
